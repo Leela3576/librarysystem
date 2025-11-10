01001f5c03977b5f46c8932cbacfb0f1cd8b3fe6
Leela3576 <akash98764625@gmail.com>
2025-11-10 20:58:06 +0530

    LMS commit and deploying

---
+++ library/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    category = models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.title
